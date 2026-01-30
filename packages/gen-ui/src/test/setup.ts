3de848964eb6017085e436b9268a15931dee574f
Jared Grimes <jaredlancegrimes@gmail.com>
2026-01-30 16:46:50 -0600

    femoved canvas

--- packages/gen-ui/src/test/setup.ts
+++ packages/gen-ui/src/test/setup.ts
@@ -1,15 +1,22 @@
 import { expect, afterEach, beforeAll } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import * as matchers from '@testing-library/jest-dom/matchers';
-import { Canvas } from 'canvas';
 
 expect.extend(matchers);
 
-// Mock canvas for OSMD
-beforeAll(() => {
-  global.HTMLCanvasElement.prototype.getContext = function() {
-    return new Canvas(200, 200).getContext('2d');
-  } as any;
+// Mock canvas for OSMD - try to use canvas package if available, otherwise use basic mock
+beforeAll(async () => {
+  try {
+    const { Canvas } = await import('canvas');
+    global.HTMLCanvasElement.prototype.getContext = function() {
+      return new Canvas(200, 200).getContext('2d');
+    } as any;
+  } catch {
+    // canvas not available, use basic mock
+    global.HTMLCanvasElement.prototype.getContext = function() {
+      return {} as any;
+    };
+  }
 });
 
 afterEach(() => {
